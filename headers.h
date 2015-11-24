78666195b1f184d2d7a91b480ff80defb038e5ed
Tomáš Růžička <t_ruzicka@email.cz>
2015-11-24 19:57:57 +0100

    Initial project commit

---
+++ headers.h
@@ -0,0 +1,122 @@
+﻿/*
+* Author: Tomáš Růžička, t_ruzicka (at) email.cz
+* 2015
+*/
+
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <windows.h>
+#include <winbio.h>
+
+typedef          int8_t    int8;
+typedef          uint8_t   uint8;
+typedef unsigned char      uchar;
+typedef          int16_t   int16;
+typedef          uint16_t  uint16;
+typedef unsigned short     ushort;
+typedef          int32_t   int32;
+typedef          uint32_t  uint32;
+typedef unsigned int       uint;
+typedef unsigned long      ulong;
+typedef          int64_t   int64;
+typedef          long long llong;
+typedef          uint64_t  uint64;
+typedef unsigned long long ullong;
+
+namespace NBmp
+{
+  static const uint32 BMP_HEADER_SIGNATURE_LENGHT = 0x02;
+  static const uint32 BMP_HEADER_SIGNATURE_LENGHT_0 = 0x03;
+  static const char   BMP_HEADER_SIGNATURE[BMP_HEADER_SIGNATURE_LENGHT_0] = "BM";
+
+  static const uint32 BMP_HEADER_RESERVED = 0x00;
+  static const uint32 BMP_HEADER_FILE_DATA_OFFSET = 0x36;
+  static const uint32 BMP_INFO_HEADER_SIZE = 0x28;
+  static const uint16 BMP_INFO_HEADER_PLANES = 0x01;
+  static const uint16 BMP_INFO_HEADER_BITS_PER_PIXEL_8 = 0x08;
+  static const uint16 BMP_INFO_HEADER_BITS_PER_PIXEL_24 = 0x18;
+  static const uint16 BMP_INFO_HEADER_BITS_PER_PIXEL_32 = 0x20;
+  static const uint32 BMP_INFO_HEADER_COMPRESSION = 0x00;
+  static const uint32 BMP_INFO_HEADER_PIXEL_PER_METER_X = 0x00;
+  static const uint32 BMP_INFO_HEADER_PIXEL_PER_METER_Y = 0x00;
+  static const uint32 BMP_INFO_HEADER_COLORS = 0x00;
+  static const uint32 BMP_INFO_HEADER_USED_COLORS = 0x00;
+
+  static const uint32 BMP_8B_COLORS = 0x01;
+  static const uint32 BMP_24B_COLORS = 0x03;
+  static const uint32 BMP_32B_COLORS = 0x04;
+}
+
+struct SColor
+{
+  uint8 r;
+  uint8 g;
+  uint8 b;
+  uint8 a;
+
+  inline SColor() : r(0), g(0), b(0), a(255) {}
+};
+
+struct SBmpInfoHeader
+{
+  uint32 headerSize;
+  uint32 width;
+  uint32 height;
+  uint16 planes;
+  uint16 bitsPerPixel;
+  uint32 compression;
+  uint32 imageSize;
+  uint32 pixelPerMeterX;
+  uint32 pixelPerMeterY;
+  uint32 colors;
+  uint32 usedColors;
+
+  inline SBmpInfoHeader() : headerSize(0), width(0), height(0), planes(0), bitsPerPixel(0), compression(0), imageSize(0), pixelPerMeterX(0), pixelPerMeterY(0), colors(0), usedColors(0) {}
+};
+
+struct SBmpImage
+{
+  std::string signature;
+  uint32 fileSize;
+  uint32 reserved;
+  uint32 dataOffset;
+  SBmpInfoHeader infoHeader;
+  std::vector<SColor> colorTable;
+  std::vector<SColor> data;
+  uint8 dataPaddingSize;
+
+  inline SBmpImage() : fileSize(0), reserved(0), dataOffset(0), dataPaddingSize(0)
+  {
+    signature.resize(NBmp::BMP_HEADER_SIGNATURE_LENGHT);
+    memset(&signature[0], 0, sizeof(char)* NBmp::BMP_HEADER_SIGNATURE_LENGHT);
+  }
+};
+
+class CFile
+{
+  private:
+  std::ofstream *out;
+
+  public:
+  inline CFile() { out = new std::ofstream(); }
+  inline CFile(std::string path) : CFile() { open(path); }
+  inline ~CFile() { delete out; }
+
+  inline void open(std::string path) { out->open(path, std::ofstream::binary); }
+  //inline void read(void *dest, uint32 bytes) const { out->read(reinterpret_cast<char *>(src), bytes); }
+  inline void write(const void *src, uint32 bytes) const { out->write(reinterpret_cast<const char *>(src), bytes); }
+  inline uint32 tell() const { out->tellp(); }
+  inline uint32 seek(int32 pos, std::ios_base::seekdir way) const { out->seekp(pos, way); }
+  inline void close() { out->close(); }
+
+  inline const std::ofstream *getFile() const { return out; }
+};
+
+#endif // HEADERS_H
---
+++ main.cpp
@@ -0,0 +1,222 @@
+﻿/*
+ * Author: Tomáš Růžička, t_ruzicka (at) email.cz
+ * 2015
+ */
+
+#include "headers.h"
+
+void BmpSetImageData(SBmpImage *bmp, const std::vector<uint8> &data, uint32 width, uint32 height)
+{
+  bmp->data.resize(data.size());
+
+  for(uint32 y = 0; y < height; y++)
+  {
+    const uint32 yy = (height - y - 1) * width;
+
+    for(uint32 x = 0; x < width; x++)
+    {
+      const uint32 xy = yy + x;
+
+      bmp->data[xy].r = data[y * width + x];
+      bmp->data[xy].g = data[y * width + x];
+      bmp->data[xy].b = data[y * width + x];
+      bmp->data[xy].a = 255;
+    }
+  }
+
+  bmp->dataPaddingSize = sizeof(uint32) - ((width * NBmp::BMP_24B_COLORS) % sizeof(uint32));
+  if(bmp->dataPaddingSize == sizeof(uint32))
+    bmp->dataPaddingSize = 0;
+
+  bmp->signature = NBmp::BMP_HEADER_SIGNATURE;
+  bmp->fileSize = NBmp::BMP_HEADER_FILE_DATA_OFFSET + bmp->colorTable.size() * NBmp::BMP_32B_COLORS + (width * NBmp::BMP_24B_COLORS + bmp->dataPaddingSize) * height;
+  bmp->reserved = NBmp::BMP_HEADER_RESERVED;
+  bmp->dataOffset = NBmp::BMP_HEADER_FILE_DATA_OFFSET + bmp->colorTable.size() * NBmp::BMP_32B_COLORS;
+  bmp->infoHeader.headerSize = NBmp::BMP_INFO_HEADER_SIZE;
+  bmp->infoHeader.width = width;
+  bmp->infoHeader.height = height;
+  bmp->infoHeader.planes = NBmp::BMP_INFO_HEADER_PLANES;
+  bmp->infoHeader.bitsPerPixel = NBmp::BMP_INFO_HEADER_BITS_PER_PIXEL_24;
+  bmp->infoHeader.compression = NBmp::BMP_INFO_HEADER_COMPRESSION;
+  bmp->infoHeader.imageSize = (width * NBmp::BMP_24B_COLORS + bmp->dataPaddingSize) * height;
+  bmp->infoHeader.pixelPerMeterX = NBmp::BMP_INFO_HEADER_PIXEL_PER_METER_X;
+  bmp->infoHeader.pixelPerMeterY = NBmp::BMP_INFO_HEADER_PIXEL_PER_METER_Y;
+  bmp->infoHeader.colors = NBmp::BMP_INFO_HEADER_COLORS;
+  bmp->infoHeader.usedColors = NBmp::BMP_INFO_HEADER_USED_COLORS;
+}
+
+void BmpSave(const SBmpImage *bmp, std::string filename)
+{
+  const uint32 dataPadding = 0;
+
+  CFile file(filename);
+
+  file.write(&bmp->signature[0], sizeof(char)* NBmp::BMP_HEADER_SIGNATURE_LENGHT);
+  file.write(&bmp->fileSize, sizeof(uint32));
+  file.write(&bmp->reserved, sizeof(uint32));
+  file.write(&bmp->dataOffset, sizeof(uint32));
+  file.write(&bmp->infoHeader.headerSize, sizeof(uint32));
+  file.write(&bmp->infoHeader.width, sizeof(uint32));
+  file.write(&bmp->infoHeader.height, sizeof(uint32));
+  file.write(&bmp->infoHeader.planes, sizeof(uint16));
+  file.write(&bmp->infoHeader.bitsPerPixel, sizeof(uint16));
+  file.write(&bmp->infoHeader.compression, sizeof(uint32));
+  file.write(&bmp->infoHeader.imageSize, sizeof(uint32));
+  file.write(&bmp->infoHeader.pixelPerMeterX, sizeof(uint32));
+  file.write(&bmp->infoHeader.pixelPerMeterY, sizeof(uint32));
+  file.write(&bmp->infoHeader.colors, sizeof(uint32));
+  file.write(&bmp->infoHeader.usedColors, sizeof(uint32));
+
+  for(uint32 i = 0; i < bmp->colorTable.size(); i++)
+  {
+    file.write(&bmp->colorTable[i].r, sizeof(uint8));
+    file.write(&bmp->colorTable[i].g, sizeof(uint8));
+    file.write(&bmp->colorTable[i].b, sizeof(uint8));
+    file.write(&bmp->colorTable[i].a, sizeof(uint8));
+  }
+
+  for(uint32 y = 0; y < bmp->infoHeader.height; y++)
+  {
+    const uint32 yy = (bmp->infoHeader.height - y - 1) * bmp->infoHeader.width;
+
+    for(uint32 x = 0; x < bmp->infoHeader.width; x++)
+    {
+      const uint32 xy = yy + x;
+
+      file.write(&bmp->data[xy].b, sizeof(uint8));
+      file.write(&bmp->data[xy].g, sizeof(uint8));
+      file.write(&bmp->data[xy].r, sizeof(uint8));
+    }
+
+    file.write(&dataPadding, sizeof(uint8)* bmp->dataPaddingSize);
+  }
+
+  file.close();
+}
+
+HRESULT CaptureSample()
+{
+  HRESULT hr = S_OK;
+  WINBIO_SESSION_HANDLE sessionHandle = NULL;
+  WINBIO_UNIT_ID unitId = 0;
+  WINBIO_REJECT_DETAIL rejectDetail = 0;
+  PWINBIO_BIR sample = NULL;
+  SIZE_T sampleSize = 0;
+
+  // Connect to the system pool. 
+  hr = WinBioOpenSession(
+    WINBIO_TYPE_FINGERPRINT,    // Service provider
+    WINBIO_POOL_SYSTEM,         // Pool type
+    WINBIO_FLAG_RAW,            // Access: Capture raw data
+    NULL,                       // Array of biometric unit IDs
+    0,                          // Count of biometric unit IDs
+    WINBIO_DB_DEFAULT,          // Default database
+    &sessionHandle              // [out] Session handle
+    );
+
+  if(FAILED(hr))
+  {
+    std::cout << "WinBioOpenSession failed. hr = 0x" << std::hex << hr << std::dec << "\n";
+
+    if(sample != NULL)
+    {
+      WinBioFree(sample);
+      sample = NULL;
+    }
+
+    if(sessionHandle != NULL)
+    {
+      WinBioCloseSession(sessionHandle);
+      sessionHandle = NULL;
+    }
+    
+    return hr;
+  }
+
+  // Capture a biometric sample.
+  std::cout << "Calling WinBioCaptureSample - Swipe sensor...\n";
+
+  hr = WinBioCaptureSample(
+    sessionHandle,
+    WINBIO_NO_PURPOSE_AVAILABLE,
+    WINBIO_DATA_FLAG_RAW,
+    &unitId,
+    &sample,
+    &sampleSize,
+    &rejectDetail
+    );
+
+  if(FAILED(hr))
+  {
+    if(hr == WINBIO_E_BAD_CAPTURE)
+      std:: cout << "Bad capture; reason: " << rejectDetail << "\n";
+    else
+      std::cout << "WinBioCaptureSample failed.hr = 0x" << std::hex << hr << std::dec << "\n";
+
+    if(sample != NULL)
+    {
+      WinBioFree(sample);
+      sample = NULL;
+    }
+
+    if(sessionHandle != NULL)
+    {
+      WinBioCloseSession(sessionHandle);
+      sessionHandle = NULL;
+    }
+
+    return hr;
+  }
+
+  std::cout << "Swipe processed - Unit ID: " << unitId << "\n";
+  std::cout << "Captured " << sampleSize << " bytes.\n";
+
+  if(sample != NULL)
+  {
+    PWINBIO_BIR_HEADER BirHeader = (PWINBIO_BIR_HEADER)(((PBYTE)sample) + sample->HeaderBlock.Offset);
+    PWINBIO_BDB_ANSI_381_HEADER AnsiBdbHeader = (PWINBIO_BDB_ANSI_381_HEADER)(((PBYTE)sample) + sample->StandardDataBlock.Offset);
+    PWINBIO_BDB_ANSI_381_RECORD AnsiBdbRecord = (PWINBIO_BDB_ANSI_381_RECORD)(((PBYTE)AnsiBdbHeader) + sizeof(WINBIO_BDB_ANSI_381_HEADER));
+
+    DWORD width = AnsiBdbRecord->HorizontalLineLength; // Width of image in pixels
+    DWORD height = AnsiBdbRecord->VerticalLineLength; // Height of image in pixels
+
+    std::cout << "Image resolution: " << width << " x " << height << "\n";
+
+    PBYTE firstPixel = (PBYTE)((PBYTE)AnsiBdbRecord) + sizeof(WINBIO_BDB_ANSI_381_RECORD);
+
+    SBmpImage bmp;
+    std::vector<uint8> data(width * height);
+    memcpy(&data[0], firstPixel, width * height);
+
+    SYSTEMTIME st;
+    GetSystemTime(&st);
+    std::stringstream s;
+    s << st.wYear << "." << st.wMonth << "." << st.wDay << "." << st.wHour << "." << st.wMinute << "." << st.wSecond << "." << st.wMilliseconds;
+    std::string bmpFile = "data/fingerPrint_"+s.str()+".bmp";
+
+    BmpSetImageData(&bmp, data, width, height);
+    BmpSave(&bmp, bmpFile);
+    //ShellExecuteA(NULL, NULL, bmpFile.c_str(), NULL, NULL, SW_SHOWNORMAL);
+
+    CFile raw("rawData.bin");
+    raw.write(&data[0], data.size());
+    raw.close();
+
+    WinBioFree(sample);
+    sample = NULL;
+  }
+
+  if(sessionHandle != NULL)
+  {
+    WinBioCloseSession(sessionHandle);
+    sessionHandle = NULL;
+  }
+
+  return hr;
+}
+
+int main()
+{
+  CreateDirectoryA("data", NULL);
+  while(!FAILED(CaptureSample()));
+}
